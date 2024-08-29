2b276abf73f57363aee92b8f3c9619e44a5897bf
Kai <kay.ritti@gmail.com>
2024-08-29 16:37:05 +1000

    backup

--- app/src/main/java/com/example/criminalintentapp/Crime.kt
+++ app/src/main/java/com/example/criminalintentapp/Crime.kt
@@ -1,9 +1,12 @@
 package com.example.criminalintentapp
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import  java.util.Date
 import java.util.UUID
 
+@Entity
 data class Crime (
-    val id: UUID,
+    @PrimaryKey val id: UUID,
     val title: String,
     val date: Date,
     val isSolved: Boolean
