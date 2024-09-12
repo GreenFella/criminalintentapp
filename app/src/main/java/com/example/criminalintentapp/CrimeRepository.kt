0e6379bff96eba14f68887864af06be936be7be5
Kai <kay.ritti@gmail.com>
2024-09-12 18:21:13 +1000

    backup

--- app/src/main/java/com/example/criminalintentapp/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintentapp/CrimeRepository.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.content.Context
 import androidx.room.Room
 import com.example.criminalintentapp.database.CrimeDatabase
+import com.example.criminalintentapp.database.migration_1_2
+import com.example.criminalintentapp.database.migration_2_3
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -23,6 +25,7 @@ class CrimeRepository private constructor(
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
+        .addMigrations(migration_1_2, migration_2_3)
         .build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
